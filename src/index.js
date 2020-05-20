c2c5313b6ad23a06bf31a7ab293f9ab6cc44ba7d
len102101 <41150588+len102101@users.noreply.github.com>
2020-05-20 19:55:41 +0900

    add mongoose

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
+const mongoose = require('mongoose')
 
 const app = express();
 const server = http.createServer(app);
 const api = require('./api');
-const { PORT : port } = process.env;
+const port  = process.env.PORT;
+const mongo_url = process.env.MONGO_URL
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(api)
+
+mongoose.connect(mongo_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(response => {
+  console.log(`Successfully connected to mongodb url: ${mongo_url}`)
+}).catch(e => {
+  console.error(e)
+})
 
 
 server.listen(port, () => {
