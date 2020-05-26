983fc50670cb68ac9972fbdd59b2285849f7265e
len102101 <41150588+len102101@users.noreply.github.com>
2020-05-26 16:16:49 +0900

    Update index.js

--- src/api/company/index.js
+++ src/api/company/index.js
@@ -2,4 +2,5 @@ const company = require("express").Router();
 const companyCtrl = require('./company.ctrl');
 const middlewareToken = require('../../middleWare/auth');
 
+console.log("Test");
 module.exports = company;
