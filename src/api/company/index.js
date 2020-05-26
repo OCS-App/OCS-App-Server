3137ae16bd950d52b1b5fb93b405e971056de266
len102101 <41150588+len102101@users.noreply.github.com>
2020-05-26 19:05:10 +0900

    test file upload

--- src/api/company/index.js
+++ src/api/company/index.js
@@ -1,5 +1,8 @@
 const company = require("express").Router();
 const companyCtrl = require('./company.ctrl');
 const middlewareToken = require('../../middleWare/auth');
+const upload = require('../../lib/uplode');
+
+company.post('/testupload', middlewareToken, upload.single('image'),companyCtrl.testupload);
 
 module.exports = company;
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const mongo_url = process.env.MONGO_URL
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(api)
+app.use('/', express.static('public'));
 
 mongoose.connect(mongo_url, {
   useCreateIndex: true,
